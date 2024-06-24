9b26146037e7d287b1f99dd6cda993fc58cd2d7e
salimboughanmi <boughanmi1salim@gmail.com>
2024-06-24 16:48:45 +0100

    taining with provider state manegment

---
+++ android/app/src/main/kotlin/com/example/eshope/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.eshope
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/testone.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class TestOne extends StatefulWidget {
+  const TestOne({super.key});
+
+  @override
+  State<TestOne> createState() => _TestOneState();
+}
+
+class _TestOneState extends State<TestOne> {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => Model(),
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text("providers"),
+            backgroundColor: Colors.amber,
+          ),
+          body: Consumer<Model>(
+            builder: (context, value, child) => Column(
+              children: [
+                Center(
+                  child: Text("${value.name}"),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    value.changeName();
+                  },
+                  textColor: Colors.white,
+                  color: Colors.blue,
+                  child: const Text(
+                    "do something",
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
+
+class Model extends ChangeNotifier {
+  String name = "welcome";
+  changeName() {
+    name = "salim";
+    notifyListeners();
+  }
+}
